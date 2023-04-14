54a7a0cde5a549be24b77d0463ac84124dae3bdc
lucasnpc <lucasnpc2@outlook.com>
2023-04-14 08:48:36 -0300

    Adding functions

---
+++ function-reference/main.hpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void calculate(float input1, float input2, char operation, float &result);
+
+void printEquation(float input1, float input2, char operation, float result);
+
+void calculate(float input1, float input2, char operation, float &result){
+    switch(operation)
+    {
+        case '+': result = input1 + input2;
+                  break;
+        case '-': result = input1 - input2;
+                  break;   
+        case '*': result = input1*  input2;
+                  break;
+        case '/': result = input1 / input2;
+                  break;
+        default:  std::cout<<"Illegal operation\n";
+    }
+}
+
+void printEquation(float input1, float input2, char operation, float result){
+    cout<<input1<<" "<<operation<<" "<<input2<<" = "<<result<<endl;
+
+}
---
+++ function-sum/main.cpp
@@ -0,0 +1,8 @@
+#include "main.hpp"
+
+int main(){
+    int m1 = 33; 
+    int m2 = 66;
+    printEquation(m1,m2,sum(m1,m2), '+');
+    return 0;
+}
